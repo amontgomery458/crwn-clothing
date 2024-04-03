76032cf7fc57513ea6e3e7a33f14414759ae3f7d
amontgomery458 <145405580+amontgomery458@users.noreply.github.com>
2024-04-03 16:01:17 -0700

    building navigation bar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
-
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  )
-}
+
 
 const Shop = () => {
   return <h1> I am the shop page</h1>
@@ -21,8 +13,9 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={ <Navigation /> }>
-        <Route path='home' element={ <Home /> } />
+        <Route index element={ <Home /> } />
         <Route path='shop' element={ <Shop /> } />
+        <Route path='sign-in' element={ <SignIn /> } />
       </Route>
     </Routes>
     );
